9c078e8c38a4f1198ae239ce87b89ba96a121fbc
Григоров Дмитрий <dmitriy.v.grigorov@gazprombank.ru>
2020-03-16 21:14:52 +0300

    add react-router to project

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import { push } from 'connected-react-router';
 import PropTypes from 'prop-types';
 import Input from 'src/components/input';
 import * as Actions from './actions';
@@ -10,11 +8,6 @@ class SignIn extends Component {
   static propTypes = {
     dataForm: PropTypes.object.isRequired,
     changeFieldAction: PropTypes.func.isRequired,
-  };
-
-  onClick = () => {
-    console.log('this.props', this.props);
-    this.props.push('/')
   };
 
   render() {
@@ -34,6 +27,42 @@ class SignIn extends Component {
         </div>
         <div>
           <div>
+            first name
+          </div>
+          <div>
+            <Input
+              id="firstname"
+              value={this.props.dataForm.firstname}
+              onChange={this.props.changeFieldAction}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            last name
+          </div>
+          <div>
+            <Input
+              id="lastname"
+              value={this.props.dataForm.lastname}
+              onChange={this.props.changeFieldAction}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            email
+          </div>
+          <div>
+            <Input
+              id="email"
+              value={this.props.dataForm.email}
+              onChange={this.props.changeFieldAction}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
             password
           </div>
           <div>
@@ -44,17 +73,13 @@ class SignIn extends Component {
             />
           </div>
         </div>
-        <button onClick={this.onClick}>Login</button>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  dataForm: state.signIn.dataForm
+  dataForm: state.signUp.dataForm
 });
 
-export default connect(mapStateToProps, {
-  push,
-  ...Actions
-})(SignIn);
+export default connect(mapStateToProps, Actions)(SignIn);
