dfb1a244796d295170cef21a7d2c7b357ab45c98
tanyazhd <zhdanovatanya96@gmail.com>
2020-03-20 23:26:53 +0300

    connect react redux

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
-
-export default class App extends Component {
+import { connect } from 'react-redux';
+import * as Actions from './actions.js';
+class App extends Component {
   render() {
     return (
       <div>
-        hello from react app
+        count = {this.props.counter}
+        <button
+          onClick={() => this.props.increaseAction(1)}
+        >
+          increase 1
+        </button>
+        <button
+          onClick={() => this.props.increaseAction(55)}
+        >
+          increase 55
+        </button>
+        <button
+          onClick={() => this.props.decreaseAction(1)}
+        >
+          decrease
+        </button>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return ({
+    counter: state.applicationReducer.counter
+  });
+};
+
+export default connect(mapStateToProps, Actions)(App);
