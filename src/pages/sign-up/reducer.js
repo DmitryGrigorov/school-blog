0fb5472ddc69d2e25c3d0ec2fdcdb36c7251b7a1
Margo <12rita43@gmail.com>
2020-03-21 22:28:30 +0300

    Sign-up page added

---
+++ src/pages/sign-up/reducer.js
@@ -0,0 +1,30 @@
+import cloneDeep from 'lodash/cloneDeep';
+
+const initState = {
+    dataForm: {
+        firstName: '',
+        login: '',
+        email: '',
+        password: ''
+    }
+};
+
+function merge(state, someObject) {
+    const clonnedState = cloneDeep(state);
+
+    return Object.assign(clonnedState, someObject);
+}
+
+export default function signUpReducer(state = initState, action) {
+    switch (action.type) {
+        case 'SIGN-UP_CHANGE_DATA_FORM':
+            return merge(state, {
+                dataForm: {
+                    ...state.dataForm,
+                    [action.payload.fieldId]: action.payload.value
+                }
+            });
+        default:
+            return state;
+    }
+}
