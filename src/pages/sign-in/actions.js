2aea7bf029135f4b40283f2040ef6de939c72549
tanyazhd <zhdanovatanya96@gmail.com>
2020-03-21 10:47:10 +0300

    add sign-in actions

---
+++ src/pages/sign-in/actions.js
@@ -0,0 +1,4 @@
+export const changeFieldAction= ({fieldId, value})=>({
+    type:'SIGN-IN_CHANGE_DATA_FORM',
+    payload: {fieldId, value} //можно просто заменить на payload. Какие поля приходят, такие и уходят
+})
