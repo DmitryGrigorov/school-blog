6c64dffab682a85c69b31ab60de4c3ec2f029ed3
tanyazhd <zhdanovatanya96@gmail.com>
2020-03-21 12:54:19 +0300

    wrap app into router

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app';
 import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
   <App />
+  </Router>
 </Provider>,
   document.getElementById('root')
 );
