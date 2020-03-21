c408ba18b5e8264155c178b7ae1a6ef9d33825d5
tanyazhd <zhdanovatanya96@gmail.com>
2020-03-21 13:34:50 +0300

    multiple middlewares

--- src/store.js
+++ src/store.js
@@ -1,12 +1,21 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import {createLogger} from 'redux-logger';
-import { connectRouter } from 'connected-react-router';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
 
 import applicationReducer from 'src/app/reducer';
 import signInReducer from 'src/pages/sign-in/reducer';
 import signUpReducer from 'src/pages/sign-up/reducer';
 import {history} from 'src/history';
 
+const logger=createLogger({
+    collapsed: true
+});
+
+const middlewares =[
+    logger, 
+    routerMiddleware(history),
+    
+]
 
 
 const createRootReducer =(history)=> combineReducers({
@@ -16,10 +25,8 @@ const createRootReducer =(history)=> combineReducers({
     signUp: signUpReducer
 });
 
-const logger=createLogger({
-    collapsed: true
-});
 
-const store=createStore(createRootReducer(history), applyMiddleware(logger)); //сюда мы можем передать только одну функцию, для этого combineReducers
+
+const store=createStore(createRootReducer(history), applyMiddleware(...middlewares)); //сюда мы можем передать только одну функцию, для этого combineReducers
 
 export default store;
