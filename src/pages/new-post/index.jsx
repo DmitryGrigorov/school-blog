ebc8e1d3a4eaa0a1e1217c5bc86ab687abba4e7b
tanyazhd <zhdanovatanya96@gmail.com>
2020-03-21 14:23:59 +0300

    add new post component, connect to store

--- src/pages/new-post/index.jsx
+++ src/pages/new-post/index.jsx
@@ -1,17 +1,41 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import Input from 'src/components/input/index';
+import PropTypes from 'prop-types';
+ import * as Actions from './actions';
 
-export default class NewPost extends Component {
+class NewPost extends Component {
+    static propTypes = {
+        dataForm: PropTypes.object.isRequired,
+        changeFieldAction: PropTypes.func.isRequired,
+      };
     render(){
         return(
             <div>
                <h1>New Post</h1> 
-            <form>
-                <div>Title</div>
-                <input type='text'></input>
-                <div>Body</div>
-                <input type='text'></input>
-            </form>
+            <div>
+                Title
+            </div>
+            <Input 
+                id='title'
+                value={this.props.dataForm.title}
+                onChange={this.props.changeFieldAction}
+            />
+            <div>
+                Body
+            </div>
+            <Input 
+                id='body'
+                value={this.props.dataForm.body}
+                onChange={this.props.changeFieldAction}
+            />
             </div>
         )
     }
 }
+
+const mapStateToProps=(state)=>({
+    dataForm: state.newPost.dataForm
+});
+
+export default connect(mapStateToProps, Actions)(NewPost);
