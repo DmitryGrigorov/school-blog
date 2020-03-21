30de186f78f2a59b1cb726a196ff31f9a923aecf
tanyazhd <zhdanovatanya96@gmail.com>
2020-03-21 10:32:03 +0300

    fix style header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import './style.css';
+import style from './style.css';
 export default class header extends Component{
     render(){
         return(
-            <div className='wrapper'>
+            <div className={style.wrapper}>
                 header
             </div>
         )
