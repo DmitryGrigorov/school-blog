c7fe844f139da835aa76d0b2760ecd4382c3360a
tanyazhd <zhdanovatanya96@gmail.com>
2020-03-21 12:51:08 +0300

    add header structure, styles

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,17 @@ export default class header extends Component{
     render(){
         return(
             <div className={style.wrapper}>
-                header
+                <ul className={style.wrapperUl}>
+                    <li><a className={style.link} href="/">Главная</a></li>
+                    <li><a className={style.link} href="/about">О сайте</a></li>
+                    <li><a className={style.link} href="/new-post">Новый пост</a></li>
+                </ul>
+
+                <ul className={style.wrapperUl}>
+                    <li><a className={style.link} href="/sign-in">Регистрация</a></li>
+                    <li><a className={style.link} href="/sign-up">Логин</a></li>
+                </ul>
+              
             </div>
         )
     }
